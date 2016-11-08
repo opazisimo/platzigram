e3cab51838dbe3087285a9a82a50f61e977470a7
opazisimo <suopazo@4talent.cl>
2016-11-08 18:14:41 -0300

    haciendo títulos dinámicos

--- src/signup/index.js
+++ src/signup/index.js
@@ -1,9 +1,11 @@
 var page = require('page');
 var empty = require('empty-element');
 var template = require('./template');
+var title = require('title');
 
 page('/signup', function(ctx, next){
   // ```` para yoyo las tildes invertidas
+  title('Platzigram - Signup');
   var main = document.getElementById('main-container');
   empty(main).appendChild(template);
 })
